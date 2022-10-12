c2b66713d20ed15345cfdb731b0ef9c868807bb2
sicmundu <grodstrike@gmail.com>
2022-10-12 13:41:26 +0400

    Add files via upload

---
+++ src/assets/test-map/barrel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="barrel" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="barrel.png" width="128" height="64"/>
+</tileset>
---
+++ src/assets/test-map/dirt.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="dirt" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="LPC Base Assets/tiles/dirt.png" width="96" height="192"/>
+ <wangsets>
+  <wangset name="test" type="corner" tile="-1">
+   <wangcolor name="dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
